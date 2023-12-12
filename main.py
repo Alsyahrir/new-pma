9df1368fa2b3dd522547decaa89373c8d8da5bcb
rioseleng09 <94964007+rioseleng09@users.noreply.github.com>
2023-12-12 19:06:55 +0700

    main.py

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ from keras.models import load_model
 from PIL import Image
 import numpy as np
 
-from util import classify, set_background
+from util import classify, set_background  # Ensure that 'util.py' is in the same directory or in the Python path
 
-
+# Set background
 set_background('./bgs/bg5.png')
 
-# set title
+# Set title
 st.title('Pneumonia classification')
 
-# set header
+# Set header
 st.header('Please upload a chest X-ray image')
 
-# upload file
+# Upload file
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
-# load classifier
+# Load classifier
 model = load_model('./model/pmacp05.h5')
 
-# load class names
+# Load class names
 with open('./model/labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
-# display image
+# Display image
 if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
-    # classify image
+    # Classify image
     class_name, conf_score = classify(image, model, class_names)
 
-    # write classification
+    # Write classification
     st.write("## {}".format(class_name))
     st.write("### score: {}%".format(int(conf_score * 1000) / 10))
